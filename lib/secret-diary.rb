f44ebaf914d648b89a295c5491088c735704d2ab
Aimée Craig <aimeecraig1@gmail.com>
2018-10-09 11:18:55 +0100

    code and spec file refactored to use multiple classes

--- lib/secret-diary.rb
+++ lib/secret-diary.rb
@@ -1,4 +1,4 @@
-class SecretDiary
+class DiaryLock
   attr_accessor :locked
 
   def initialize
@@ -13,12 +13,16 @@ class SecretDiary
   def unlock
     @locked = false
   end
+end
 
+class DiaryEntries
   def add_entry
     raise 'Unable to add entry, diary is locked' if @locked == true
     true
   end
+end
 
+class DiaryDisplay
   def get_entries
     raise 'Cannot get entries, diary is locked.' if @locked == true
     true
--- spec/secret-diary_spec.rb
+++ spec/secret-diary_spec.rb
@@ -1,30 +1,34 @@
 require 'secret-diary'
 
-describe SecretDiary do
-  it 'raises an error when trying to add_entry to a new diary' do
-    expect { subject.add_entry }.to raise_error 'Unable to add entry, diary is locked'
-  end
-
+describe DiaryLock do
   it 'returns false when calling unlock' do
     expect(subject.unlock).to eq false
   end
 
+  it 'raises an error when trying to lock a locked diary' do
+    expect { subject.lock }.to raise_error 'Diary is already locked.'
+  end
+end
+
+describe DiaryEntries do
   it 'returns true when calling add_entry' do
     subject.unlock
     expect(subject.add_entry).to eq true
   end
 
-  it 'returns true when calling get_entries' do
-    subject.unlock
-    expect(subject.get_entries).to eq true
+  it 'raises an error when trying to add_entry to a new diary' do
+    expect { subject.add_entry }.to raise_error 'Unable to add entry, diary is locked'
   end
 
   it 'raises an error when trying to add_entry to locked diary' do
     expect { subject.add_entry }.to raise_error 'Unable to add entry, diary is locked'
   end
+end
 
-  it 'raises an error when trying to lock a locked diary' do
-    expect { subject.lock }.to raise_error 'Diary is already locked.'
+describe DiaryDisplay do
+  it 'returns true when calling get_entries' do
+    subject.unlock
+    expect(subject.get_entries).to eq true
   end
 
   it 'raises an error when trying to get_entries from a locked diary' do
